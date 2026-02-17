d759993c0542d446961c29581df968cd56352c33
Gana225 <chganapathi4225@gmail.com>
2026-02-17 12:06:08 +0530

    all done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import {SiteDataProvider} from "./context/SiteDataContext"
 
@@ -9,14 +9,14 @@ import {SiteDataProvider} from "./context/SiteDataContext"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <SiteDataProvider>
-      <Router future={{
+      <HashRouter future={{
           v7_startTransition: true,
           v7_relativeSplatPath: true,
         }}>
         <div className="bg-royal-900 text-white min-h-screen font-body selection:bg-royal-gold selection:text-black">
           <App />
         </div>
-      </Router>
+      </HashRouter>
     </SiteDataProvider>
   </StrictMode>
 );
